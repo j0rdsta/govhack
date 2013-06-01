2969f6cd44dbbf74712fdd559d007dbd3f3d50f0
Matt Patterson <matt.patterson94@gmail.com>
2013-06-01 13:03:55 +1000

    Converted Slice to PHP and templated with smarty
    Also put graph javascript into external js file called graph.js

---
+++ app/templates_c/749422d4cfc3eb5677cf499730392b6accd4d1c7.file.index.tpl.php
@@ -0,0 +1,186 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2013-06-01 04:51:59
+         compiled from ".\templates\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:3186751a960196b0c34-46223951%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '749422d4cfc3eb5677cf499730392b6accd4d1c7' => 
+    array (
+      0 => '.\\templates\\index.tpl',
+      1 => 1370055035,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3186751a960196b0c34-46223951',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_51a9601996b535_66003847',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51a9601996b535_66003847')) {function content_51a9601996b535_66003847($_smarty_tpl) {?>
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Full Stop - Home</title>
+
+  <link rel="stylesheet" href="stylesheets/normalize.css" />
+
+  <link rel="stylesheet" href="stylesheets/app.css" />
+
+  <script src="javascripts/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+	<!-- Navigation -->
+	<div class="contain-to-grid">
+		<nav class="top-bar">
+			<ul class="title-area">
+				<!-- Title Area -->
+				<li class="name">
+					<h1>
+						<a href="#">
+							Full Stop
+						</a>
+					</h1>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
+	<!-- End Top Bar -->
+
+	<!-- Search -->
+	<div class="row">
+		<div class="row">
+			<div class="small-2 columns">
+				<label for="search" class="right inline">Search for a location</label>
+			</div>
+			<div class="small-10 columns">
+				<input type="text" id="search" placeholder="eg. Varsity Lakes">
+			</div>
+		</div>
+	</div>
+	<hr>
+	<!-- End Search -->
+
+	<!-- Item -->
+	<a href="#">
+		<div class="row">
+			<div class="small-2 columns">
+				<img src="http://placehold.it/100" alt="">
+			</div>
+			<div class="small-10 columns">
+				<h5>Varsity Lakes</h5>
+				<p>Crime Rating: Low<br>
+				Growth Rate: High</p>
+			</div>
+		</div>
+	</a>
+	<hr>
+	<!-- End Item -->
+
+	<!-- Item -->
+	<a href="#">
+		<div class="row">
+			<div class="small-2 columns">
+				<img src="http://placehold.it/100" alt="">
+			</div>
+			<div class="small-10 columns">
+				<h5>Varsity Lakes</h5>
+				<p>Crime Rating: Low<br>
+				Growth Rate: High</p>
+			</div>
+		</div>
+	</a>
+	<hr>
+	<!-- End Item -->
+
+	<!-- Item -->
+	<a href="#">
+		<div class="row">
+			<div class="small-2 columns">
+				<img src="http://placehold.it/100" alt="">
+			</div>
+			<div class="small-10 columns">
+				<h5>Varsity Lakes</h5>
+				<p>Crime Rating: Low<br>
+				Growth Rate: High</p>
+			</div>
+		</div>
+	</a>
+	<hr>
+	<!-- End Item -->
+
+	<!-- Item -->
+	<a href="#">
+		<div class="row">
+			<div class="small-2 columns">
+				<img src="http://placehold.it/100" alt="">
+			</div>
+			<div class="small-10 columns">
+				<h5>Varsity Lakes</h5>
+				<p>Crime Rating: Low<br>
+				Growth Rate: High</p>
+			</div>
+		</div>
+	</a>
+	<hr>
+	<!-- End Item -->
+
+	<div class="row">
+		<div class="large-12 columns">
+			<p>
+				<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">FullStop</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">GovHat</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.
+				<br>
+				<a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/80x15.png" /></a>
+			</p>
+		</div>
+	</div>
+
+	<script>
+	document.write('<script src=' +
+		('__proto__' in {} ? 'javascripts/vendor/zepto' : 'javascripts/vendor/jquery') +
+		'.js><\/script>')
+	</script>
+
+	<script src="javascripts/foundation/foundation.js"></script>
+	
+	<script src="javascripts/foundation/foundation.alerts.js"></script>
+	
+	<script src="javascripts/foundation/foundation.clearing.js"></script>
+	
+	<script src="javascripts/foundation/foundation.cookie.js"></script>
+	
+	<script src="javascripts/foundation/foundation.dropdown.js"></script>
+	
+	<script src="javascripts/foundation/foundation.forms.js"></script>
+	
+	<script src="javascripts/foundation/foundation.joyride.js"></script>
+	
+	<script src="javascripts/foundation/foundation.magellan.js"></script>
+	
+	<script src="javascripts/foundation/foundation.orbit.js"></script>
+	
+	<script src="javascripts/foundation/foundation.placeholder.js"></script>
+	
+	<script src="javascripts/foundation/foundation.reveal.js"></script>
+	
+	<script src="javascripts/foundation/foundation.section.js"></script>
+	
+	<script src="javascripts/foundation/foundation.tooltips.js"></script>
+	
+	<script src="javascripts/foundation/foundation.topbar.js"></script>
+	
+
+	<script>
+	$(document).foundation();
+	</script>
+</body>
+</html>
+<?php }} ?>
