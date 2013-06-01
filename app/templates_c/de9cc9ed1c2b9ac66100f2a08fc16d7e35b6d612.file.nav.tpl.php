e5c4a201f132477023f7d81aa69c6eb1620daa95
Matt Patterson <matt.patterson94@gmail.com>
2013-06-01 19:24:40 +1000

    Started suburbs page
    Changed some scss

--- app/templates_c/de9cc9ed1c2b9ac66100f2a08fc16d7e35b6d612.file.nav.tpl.php
+++ app/templates_c/de9cc9ed1c2b9ac66100f2a08fc16d7e35b6d612.file.nav.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-06-01 08:12:39
+<?php /* Smarty version Smarty-3.1.13, created on 2013-06-01 10:34:30
          compiled from ".\templates\includes\nav.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:843451a967f1ec8a08-22740583%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'de9cc9ed1c2b9ac66100f2a08fc16d7e35b6d612' => 
     array (
       0 => '.\\templates\\includes\\nav.tpl',
-      1 => 1370066953,
+      1 => 1370071458,
       2 => 'file',
     ),
   ),
@@ -17,9 +17,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'version' => 'Smarty-3.1.13',
   'unifunc' => 'content_51a967f1eca2d8_89876707',
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_51a967f1eca2d8_89876707')) {function content_51a967f1eca2d8_89876707($_smarty_tpl) {?><body>
+<?php if ($_valid && !is_callable('content_51a967f1eca2d8_89876707')) {function content_51a967f1eca2d8_89876707($_smarty_tpl) {?><body <?php if ($_smarty_tpl->tpl_vars['title']->value=="FullStop"){?>class='home'<?php }?>>
 
 	<!-- Navigation -->
 	<div class="contain-to-grid">
@@ -36,28 +40,24 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 				<li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
 			</ul>
 
-			<section class="top-bar-section">
-				<!-- Left Nav Section -->
-				<ul class="left">
-					<li class="divider"></li>
-					<li><a href="#">Suburbs</a>
-					</li>
-				</ul>
-				<!-- Right Nav Section -->
-				<ul class="right">
-					<li class="divider"></li>
-					<li class="has-form">
-						<a class="button" href="#">Review</a>
-					</li>
-				</ul>
-			</section>
+			<?php if ($_smarty_tpl->tpl_vars['title']->value!="FullStop"){?>
+				<section class="top-bar-section">
+					<!-- Right Nav Section -->
+					<ul class="right">
+						<li class="divider"></li>
+						<li class="has-form">
+							<a class="button" href="#">Review</a>
+						</li>
+					</ul>
+				</section>
+			<?php }?>
 		</nav>
 	</div>
 
 	<!-- End Top Bar -->
 
-	<?php echo $_smarty_tpl->getSubTemplate ("feature.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
-
+	<?php if ($_smarty_tpl->tpl_vars['title']->value=="FullStop"){?><?php echo $_smarty_tpl->getSubTemplate ("feature.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+<?php }?>
 
 	<!-- Search -->
 	<div class="row">
