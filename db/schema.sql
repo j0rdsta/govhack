2fafb8bb25e31430dec1918e4a4b673a818465a0
Bentley O'Kane-Chase <bentleyo@gmail.com>
2013-06-02 13:50:42 +1000

    Add email field

--- db/schema.sql
+++ db/schema.sql
@@ -28,6 +28,7 @@ CREATE TABLE `reviews` (
   `review_id` int(11) NOT NULL,
   `suburb_id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL,
+  `email` varchar(256) NOT NULL,
   `review` text NOT NULL,
   `ip_address` varchar(50) NOT NULL,
   `rating` tinyint(1) unsigned NOT NULL,
